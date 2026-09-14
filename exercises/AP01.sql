1d4898ce7668bd6267bc4cadc3c2c1782e6ea704
Paulo Barata <115508072+paulobarata20@users.noreply.github.com>
2026-09-14 13:11:18 +0000

    AP01:Introdução ao SQL

---
+++ exercises/AP01.sql
@@ -0,0 +1,28 @@
+/*
+SELECT 'Olá, SQL' AS mensagem;
+
+SELECT
+    2+3 as soma,
+    10*4 as produto,
+    9.0/2 as divisão;
+
+SELECT
+    DB_NAME() as base_atual,
+    GETDATE() as agora;
+
+SELECT TOP 5 * FROM HR.EMPLOYEES;
+
+SELECT TOP 5 
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    LAST_NAME
+ FROM HR.EMPLOYEES;
+*/
+
+SELECT 'Estou pronto para SQL' as estado;
+
+SELECT 3 * 12.5 AS RESULTADO;
+
+SELECT TOP 5 
+    DEPARTMENT_ID
+FROM HR.EMPLOYEES
